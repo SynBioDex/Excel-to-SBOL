f786485a255ac8b1f0115cfd44509520538b184b
IsabelMarleen <52419857+IsabelMarleen@users.noreply.github.com>
2020-09-29 16:36:10 +0200

    Update setup.py

--- excel2sbol/setup.py
+++ excel2sbol/setup.py
@@ -9,8 +9,8 @@ Created on Fri Sep 25 16:41:00 2020
 from setuptools import setup, find_packages
  
 setup(name='excel2sbol',
-      version='0.1.0-alpha',
-      url='https://github.com/SynBioDex/Excel-to-SBOL',
+      version='0.1.0-test',
+      url='https://github.com/SynBioDex/Excel-to-SBOL/tree/master/excel2sbol',
       license='BSD 3-clause',
       author='',
       description='convert excel templates into sbol',
