fe5a3070c326600c677f2e98b2e4c6df79b43629
IsabelMarleen <52419857+IsabelMarleen@users.noreply.github.com>
2020-09-30 22:05:48 +0200

    Update setup.py
    Added maintainer info

--- excel2sbol/setup.py
+++ excel2sbol/setup.py
@@ -9,10 +9,11 @@ Created on Fri Sep 25 16:41:00 2020
 from setuptools import setup, find_packages
  
 setup(name='excel2sbol',
-      version='0.1.0-test',
+      version='0.1.0-alpha',
       url='https://github.com/SynBioDex/Excel-to-SBOL/tree/master/excel2sbol',
       license='BSD 3-clause',
-      author='',
+      maintainer='Tramy Nguyen',
+      maintainer_email='tramy.nguy@gmail.com',
       description='convert excel templates into sbol',
       packages=find_packages(include=[‘excel2sbol’]),
       long_description=open('README.md').read(),
