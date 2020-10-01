0273c45823b6cd3172b217b2bfe9816c1092ec2d
IsabelMarleen <52419857+IsabelMarleen@users.noreply.github.com>
2020-10-01 08:39:01 +0200

    Update setup.py
    New version

--- excel2sbol/setup.py
+++ excel2sbol/setup.py
@@ -9,7 +9,7 @@ Created on Fri Sep 25 16:41:00 2020
 from setuptools import setup, find_packages
  
 setup(name='excel2sbol',
-      version='0.1.0-alpha',
+      version='1.0.0-alpha',
       url='https://github.com/SynBioDex/Excel-to-SBOL/tree/master/excel2sbol',
       license='BSD 3-clause',
       maintainer='Tramy Nguyen',
