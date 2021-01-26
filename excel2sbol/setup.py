4cc5a846f9405b0a3ae68242753a325ec634192f
JMante1 <40006518+JMante1@users.noreply.github.com>
2021-01-26 10:10:42 -0700

    Update setup.py

--- excel2sbol/setup.py
+++ excel2sbol/setup.py
@@ -8,7 +8,7 @@ Created on Fri Sep 25 16:41:00 2020
 
 from setuptools import find_packages, setup
 
-setup(name='utils',
+setup(name='excel2sbol',
       version='1.0.0-alpha',
       url='https://github.com/SynBioDex/Excel-to-SBOL/tree/master/excel2sbol',
       license='BSD 3-clause',
