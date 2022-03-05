da093bd958933005a2e6c86347a7f7f83b3f27f0
Helen Scott <helengracescott@gmail.com>
2022-03-04 22:04:55 -0500

    make function callable

--- excel2sbol/excel2sbol/converter.py
+++ excel2sbol/excel2sbol/converter.py
@@ -1,25 +1,25 @@
 import os
 import excel2sbol.compiler_test as e2s
 
+def converter(file_path_in, file_path_out, sbol_version=3):
+    """Convert a given excel file to SBOL
+    
+    Args:
+        file_path_in (string): path to excel file
+        file_path_out (string): desired path to sbol file
+        sbol_version (int): sbol version number, defaults to 3
+    """
+    col_read_df, to_convert, compiled_sheets = e2s.initialise(file_path_in)
 
-# the homespace only works if the change is made to pysbol2 shown
-# in https://github.com/SynBioDex/pySBOL2/pull/411/files
-cwd = os.getcwd()
-file_path_in = os.path.join(cwd, 'excel2sbol', 'tests', 'test_files',
-                            'pichia_comb_dev_compiler_sbol3.xlsx')
-file_path_out = os.path.join(cwd, 'out.xml')
-sbol_version = 3
+    if sbol_version == 2:
+        doc, dict_of_objs, sht_convert_dict = e2s.parse_objects(col_read_df,
+                                                                to_convert,
+                                                                compiled_sheets)
+    elif sbol_version == 3:
+        doc, dict_of_objs, sht_convert_dict = e2s.parse_objects3(col_read_df,
+                                                                to_convert,
+                                                                compiled_sheets)
 
-col_read_df, to_convert, compiled_sheets = e2s.initialise(file_path_in)
-
-if sbol_version == 2:
-    doc, dict_of_objs, sht_convert_dict = e2s.parse_objects(col_read_df,
-                                                            to_convert,
-                                                            compiled_sheets)
-elif sbol_version == 3:
-    doc, dict_of_objs, sht_convert_dict = e2s.parse_objects3(col_read_df,
-                                                            to_convert,
-                                                            compiled_sheets)
-
-e2s.column_parse(to_convert, compiled_sheets, sht_convert_dict, dict_of_objs,
-                 col_read_df, doc, file_path_out, sbol_version=sbol_version)
+    e2s.column_parse(to_convert, compiled_sheets, sht_convert_dict,
+                     dict_of_objs, col_read_df, doc, file_path_out,
+                     sbol_version=sbol_version)
