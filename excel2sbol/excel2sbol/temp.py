ddf59416cd0753b2d467781fe222c0fccba679f4
JMante1 <40006518+JMante1@users.noreply.github.com>
2022-04-12 15:25:38 -0600

    latest

--- excel2sbol/excel2sbol/temp.py
+++ excel2sbol/excel2sbol/temp.py
@@ -3,7 +3,7 @@ import os
 
 cwd = os.getcwd()
 print(cwd)
-file_path_in = os.path.join(cwd, "excel2sbol", 'tests', 'test_files', 'test_version5_flapjack_compiler_sbol3_v007.xlsx')
+file_path_in = os.path.join(cwd, "excel2sbol", 'tests', 'test_files', 'test_version7_flapjack_compiler_sbol3_v0021.xlsx')
 file_path_out = os.path.join(cwd, "excel2sbol", 'tests', 'test_files', 'out.xml')
 
 
