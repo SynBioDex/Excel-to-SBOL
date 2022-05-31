d67499ad0d57513c86666333ed746d853cc9bcb6
JMante1 <40006518+JMante1@users.noreply.github.com>
2022-05-31 12:06:53 -0600

    update templates

--- excel2sbol/excel2sbol/temp.py
+++ excel2sbol/excel2sbol/temp.py
@@ -3,8 +3,8 @@ import os
 
 cwd = os.getcwd()
 print(cwd)
-file_path_in = os.path.join(cwd, "excel2sbol", 'tests', 'test_files', 'pichia_comb_dev_compiler_sbol3_v006.xlsx')
-file_path_out = os.path.join(cwd, "excel2sbol", 'tests', 'test_files', 'sbol_utilities.xml')
+file_path_in = os.path.join(cwd, "excel2sbol", 'tests', 'test_files', 'pichia_comb_dev_compiler_sbol3_v007.xlsx')
+file_path_out = os.path.join(cwd, "excel2sbol", 'tests', 'test_files', 'sbol_v7.xml')
 
 
 conv.converter(file_path_in, file_path_out, sbol_version=2)
