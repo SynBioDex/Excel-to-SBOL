0256a2cd1a156e317ff27a86c579a6d4f916dfd2
JMante1 <40006518+JMante1@users.noreply.github.com>
2022-07-22 11:57:42 -0600

    sbol3 up to scratch

--- excel2sbol/excel2sbol/temp3.py
+++ excel2sbol/excel2sbol/temp3.py
@@ -1,19 +1,19 @@
-import sbol3
+# import sbol3
 
-doc = sbol3.Document()
+# doc = sbol3.Document()
 
-colec = sbol3.Collection('FinalProducts', name='FinalProducts')
-doc.add(colec)
+# colec = sbol3.Collection('FinalProducts', name='FinalProducts')
+# doc.add(colec)
 
-# print(sbol3.get_namespace())
+# # print(sbol3.get_namespace())
 
-sbol_objs = doc.objects
-sbol_objs_names = [x.name for x in sbol_objs]
-if 'FinalProducts' not in sbol_objs_names:
-    colec = sbol3.Collection('FinalProducts', name='FinalProducts')
-    # colec.members.append('test')
-    doc.add(colec)
-else:
-    colec = sbol_objs[sbol_objs_names.index('FinalProducts')]
+# sbol_objs = doc.objects
+# sbol_objs_names = [x.name for x in sbol_objs]
+# if 'FinalProducts' not in sbol_objs_names:
+#     colec = sbol3.Collection('FinalProducts', name='FinalProducts')
+#     # colec.members.append('test')
+#     doc.add(colec)
+# else:
+#     colec = sbol_objs[sbol_objs_names.index('FinalProducts')]
 
-colec.members.append('this')
+# colec.members.append('this')
